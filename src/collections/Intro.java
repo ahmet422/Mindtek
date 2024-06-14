9e473b25c6b6485edbf4cb395f7ade112fe56a2a
ahmet422 <ahmet.tajik@icloud.com>
2024-06-13 19:18:16 -0500

    solved a stack problem

--- src/collections/Intro.java
+++ src/collections/Intro.java
@@ -9,22 +9,37 @@ public class Intro {
         list.add(5);
 
         Stack<Integer> stack = new Stack<>();
-        System.out.println(stack);
+    //    System.out.println(stack);
         // add : push(el)
         stack.push(5);
         stack.push(7);
         stack.push(2);
-        System.out.println(stack);
-        System.out.println(stack.pop());
-        System.out.println(stack);
+//        System.out.println(stack);
+//        System.out.println(stack.pop());
+//        System.out.println(stack);
 
-
+        Stack<String> words = new Stack<>();
+        System.out.println(words);
+        if(!words.isEmpty()) words.peek();
 
 
     }
 
     public static boolean isValid(String s) {
-
-        return false;
+        // if opening then put into the stack
+        // else get the el from stack and check types:
+                // if types match then continue
+                // else return false
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            char par = s.charAt(i);
+            if(par=='(' || par=='[' || par=='{') stack.push(par);
+            else{
+                if(stack.isEmpty()) return false;
+                char el = stack.pop();
+                if(par == ')' && el != '(' || par == ']' && el != '[' || par == '}' && el != '{' ) return false;
+            }
+        }
+        return stack.isEmpty();
     }
 }
